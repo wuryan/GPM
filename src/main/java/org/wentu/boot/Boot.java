5d899b07661d0d61518ec806c9cb33eb731b8329
wuryan <wuguowen666@outlook.com>
2016-12-27 14:25:07 +0800

    提交测试

--- src/main/java/org/wentu/boot/Boot.java
+++ src/main/java/org/wentu/boot/Boot.java
@@ -1,9 +1,15 @@
 package org.wentu.boot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by wuguo on 2016/8/8.
  */
-
+@SpringBootApplication
 public class Boot{
 
+    public static void main(String[] args) {
+        SpringApplication.run(Boot.class);
+    }
 }
--- src/test/java/org/wentu/test/LifeCycle.java
+++ src/test/java/org/wentu/test/LifeCycle.java
@@ -1,7 +1,29 @@
 package org.wentu.test;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 /**
  * Created by wuguo on 2016/12/25.
  */
-public class LifeCycle {
+public class LifeCycle implements BeanNameAware, BeanFactoryAware, ApplicationContextAware{
+
+    @Override
+    public void setBeanName(String s) {
+
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
 }
